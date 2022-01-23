abf2b7bb65e66e1c957f6531fcaf7574c1b0f06f
encryted-chap <Encrypted-Chap@protonmail.com>
2022-01-22 22:12:18 -0500

    .

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-g++ -I"src/include" -L"src/lib" -o out.exe main.cpp -lmingw32 -lSDL2main -lSDL2
+g++ -I"sdl/include" -L"sdl/lib" -o out.exe main.cpp -lmingw32 -lSDL2main -lSDL2
+./out.exe
+rm out.exe # keep 'er clean
